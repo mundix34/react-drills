a203b1b06806352576584f1467bd4c39069b5522
Your Name <you@example.com>
2018-07-02 17:22:29 -0600

    one and two complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      array:[
+        'Today', 
+        'Tomorrow', 
+        'Forever']
+    }
+  }
   render() {
+    let displayArray= this.state.array.map((e,i)=>{
+      return (
+      <h2 key={i}>{e}</h2>
+    )
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  
+        { displayArray }
+          
       </div>
     );
   }
