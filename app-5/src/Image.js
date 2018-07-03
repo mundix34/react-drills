125020a22f8107970ca113a7448799838a230c01
Your Name <you@example.com>
2018-07-02 20:07:18 -0600

    app-5 completed

---
+++ app-5/src/Image.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+export default function (props){
+    return (
+        <div>
+            <img src = {props.myImage} alt = 'cats fighting'/>
+        </div>
+    )
+
+}
