c1bb135aedbdf4d9af715db1ad61fb691561b0c7
Your Name <you@example.com>
2018-07-02 21:40:26 -0600

    done with app-6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
+
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      text:'',
+      array:[]
+    }
+    this.handleArray=this.handleArray.bind(this);
+  
+  }
+  handleText(val){
+    this.setState({text:val})
+  }
+  handleArray(){
+    this.setState({
+      array:[...this.state.array, this.state.text],
+      text:''
+    })
+  }
   render() {
+    let array=this.state.array.map((element,index)=>{
+      return (
+      <Todo key={index} task={element}/>
+      )
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>My to-do list:</h1>
+      <div>
+        <input value ={this.state.text}
+        placeholder='Enter text'
+         onChange={(e)=>this.handleText(e.target.value)}
+         />
+        <button onClick={this.handleArray}>Add</button>
+        
+      </div>
+      <br/>
+      {array}
       </div>
     );
   }
